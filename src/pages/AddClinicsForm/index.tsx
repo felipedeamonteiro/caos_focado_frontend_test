ae4f30ebaef1e900bc117f1c4885dc7aa6e5ba43
Felipe Monteiro <felipedeamonteiro@gmail.com>
2021-01-03 01:51:01 -0300

    Add raw form to form page

--- src/pages/AddClinicsForm/index.tsx
+++ src/pages/AddClinicsForm/index.tsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
 import { Container } from './styles';
+import Input from '../../components/Input';
+import Checkbox from '../../components/Checkbox';
+import Button from '../../components/Button';
+
+interface CheckboxOption {
+  id: string;
+  value: string;
+  label: string;
+}
 
 const AddClinicsForm: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const checkboxOptions: CheckboxOption[] = [
+    {
+      id: 'exames_clinicos',
+      value: 'Exames Clínicos',
+      label: 'Exames Clínicos',
+    },
+    {
+      id: 'ex_complementares',
+      value: 'Exames Complementares',
+      label: 'Exames Complementares',
+    },
+    { id: 'ppra', value: 'PPRA', label: 'PPRA' },
+    { id: 'pcmso', value: 'PCMSO', label: 'PCMSO' },
+  ];
+
+  const handleSubmit = useCallback(data => {
+    console.log('data', data);
+  }, []);
+
   return (
     <Container>
-      <h1>Form</h1>
+      <Form ref={formRef} onSubmit={handleSubmit} id="form">
+        <div className="left-div">
+          <Input
+            name="clinicName"
+            placeholder="Digite o nome da clínica"
+            label="Nome da Clínica"
+          />
+          <Input name="cep" placeholder="Digite o cep da clínica" label="CEP" />
+          <Input
+            name="address"
+            placeholder="Endereço"
+            label="Endereço da Clínica"
+          />
+        </div>
+        <div className="right-div">
+          <Input name="email" placeholder="Digite o email" label="Email" />
+          <Input
+            name="whatsapp"
+            placeholder="Digite o WhatsApp"
+            label="WhatsApp"
+          />
+          <Checkbox name="checkbox" options={checkboxOptions} />
+        </div>
+        <div className="button-div">
+          <Button type="submit">Adicionar</Button>
+        </div>
+      </Form>
     </Container>
   );
 };
